9c439b97275499a399b19ce6f55e7c471f5d71c2
Nandhi Pandiaraj <nandhirajan.pandiaraj@byjus.com>
2020-06-20 23:38:15 +0530

    Rearranged folder structure

---
+++ src/screens/MealDetailScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const MealDetailScreen = props => {
+    return (
+        <View>
+            <Text>
+                Meal Detail Screen
+            </Text>
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({});
+
+export default MealDetailScreen;
