3f6ab506d95ff7b4c570c0148fd5bf8d3e3528bf
Nandhi Pandiaraj <nandhirajan.pandiaraj@byjus.com>
2020-06-28 12:58:54 +0530

    Adding header button

--- src/screens/MealDetailsScreen.js
+++ src/screens/MealDetailsScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
+import CustomHeaderButton from "../components/CustomHeaderButton";
 import { MEALS } from "../data/dummy-data";
 
 const MealDetailsScreen = props => {
@@ -24,11 +26,18 @@ MealDetailsScreen.navigateOptions = (navigationData) => {
     const mealId = navigationData.navigation.getParams("mealId");
     const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
-    return(
-        {
-            headerTitle: selectedMeal.title
-        }
-    )
+    return {
+        headerTitle: selectedMeal.title,
+        headerRight:
+            <HeaderButtons HeaderButtonComponent ={CustomHeaderButton}>
+                <Item
+                    title="Favourite"
+                    iconName="ios-star"
+                    onPress={() => {
+                        console.log("you clicked Favourite");
+                    }} />
+            </HeaderButtons>
+    }
 }
 
 const styles = StyleSheet.create({
